67bd70db269627246d9fdb51bb95c139a589e9e3
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2022-08-17 18:45:59 -0500

    Update streamlit_app.py
    Updated index to fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
-import pandas
+
 import streamlit
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -10,6 +9,10 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.text('🍌🥭Build Your Own Smoothie🥝🍇')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to incluye
 streamlit.multiselect("Pick sone fruits:", list(my_fruit_list.index))
