be1a6d10cca9470a9bec81d674a09d484d9bdf11
Salvador Lopez <46700662+svvyse@users.noreply.github.com>
2022-08-17 18:11:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
-
+import pandas
 import streamlit
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
@@ -9,4 +10,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.text('🍌🥭Build Your Own Smoothie🥝🍇')
+streamlit.dataframe(my_fruit_list)
 
